8950a0d1a9dcb4e17d6eee090eb2e03266da22b2
GGyong <84078044+jgy1231@users.noreply.github.com>
2022-02-15 03:35:14 +0900

    Header update_1

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import logo_white from "../asset/header/logo_white.png";
 import profile_white from "../asset/header/profile_white.png";
 import { NavLink, withRouter } from "react-router-dom";
+import "../asset/css/Header.css"
 
 const Header = ({ location, match, history }) => {
   return (
@@ -21,15 +22,31 @@ const Header = ({ location, match, history }) => {
             </NavLink>
           </div>
           <div className="styled_menu">
-            <NavLink className="Menu" to="/about">
-              동아리
-            </NavLink>
+            <div className="dropdown">
+              <NavLink className="Menu" to="/about">
+                동아리
+              </NavLink>
+              <div className="dropdown-content">
+                <a href="#">소개</a>
+                <a href="#">연혁</a>
+                <a href="#">집부</a>
+              </div>
+            </div>
             <NavLink className="Menu" to="/gallery">
               갤러리
             </NavLink>
-            <NavLink className="Menu" to="/board">
-              게시판
-            </NavLink>
+            <div className="dropdown">
+              <NavLink className="Menu" to="/board">
+                게시판
+              </NavLink>
+              <div className="dropdown-content">
+                <a href="#">공지사항</a>
+                <a href="#">자유게시판</a>
+                <a href="#">정보게시판</a>
+                <a href="#">질문게시판</a>
+                <a href="#">풍사</a>
+              </div>
+            </div>
             <NavLink className="Menu" to="/pungmul">
               풍물
             </NavLink>
